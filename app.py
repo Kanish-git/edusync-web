0be1ef2d82bec737534d905b6c82365e21a384a3
Kanish Patrick <kanishpatrick@gmail.com>
2026-05-18 11:11:58 +0530

    added database dashboard

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+# DATABASE
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config["SQLALCHEMY_DATABASE_URI"] = \
+    "sqlite:///" + os.path.join(basedir, "database.db")
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# MODEL
+class Student(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    student_name = db.Column(db.String(100))
+
+    register_number = db.Column(db.String(50))
+
+    status = db.Column(db.String(50))
+
+
+# HOME
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    students = Student.query.all()
+
+    return render_template(
+        "index.html",
+        students=students
+    )
+
+
+# CREATE DATABASE
+with app.app_context():
+    db.create_all()
+
 
 if __name__ == "__main__":
     app.run()
