a778104fe1a818c945b7119ace9f02a34fa53cc0
Kanish Patrick <kanishpatrick@gmail.com>
2026-05-18 10:06:39 +0530

    vercel setup

--- app.py
+++ app.py
@@ -28,3 +28,5 @@ if __name__ == '__main__':
     
     # host='0.0.0.0' allows access over your local Wi-Fi network
     app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
